40434fc3f6583630ff78476b3e358a3db8f58105
Edielynne <barnesedielynne171999@gmail.com>
2023-02-13 02:45:03 +0800

    added information

--- src/Info.js
+++ src/Info.js
@@ -5,13 +5,13 @@ const features = [
     {
       name: 'UI/UX',
       description:
-        'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+        'I can help create a nice interface for an app,website or other interactive media and i mainly prefer using tailwind for UI/UX Design.',
       icon: CodeBracketSquareIcon,
     },
     {
       name: 'Front end ',
       description:
-        'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+        'I will be responsible for maintaining and improving a website and using a variety of markup languages and also developing new user-facing features, collaborating with the team to improve the usability.',
       icon: HeartIcon,
     },
     
@@ -22,15 +22,19 @@ const Info = () => {
         <div className="bg-white py-2 sm:py-2">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl lg:text-center">
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                What do I do?
+              <p className=" mt-1 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl ">
+                Who am i?
               </p>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
-                pulvinar et feugiat blandit at. In mi viverra elit nunc.
+              <p className="mt-6 text-lg text-justify   text-gray-600 pb-10 ">
+               
+I am a graduate of Associate in Computer Technologies with different job experiences like SMM, Customer Service Clerk, and Data Entry Clerk. Recently I decided to take a second chance to pursue a career in technology and am currently enrolled in StackTrek Bootcamp to become a Full Stack Developer.
+              </p>
+              
+              <p className="border-t mt-2  text-3xl pt-10 font-bold tracking-tight text-gray-900 sm:text-4xl">
+                What do i do?
               </p>
             </div>
-            <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+            <div className="mx-auto mt-10 max-w-2xl sm:mt-15 lg:mt-6 lg:max-w-4xl">
               <dl className="grid max-w-xl grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
                 {features.map((feature) => (
                   <div key={feature.name} className="relative  bg-gray-200  rounded-2xl p-3">
